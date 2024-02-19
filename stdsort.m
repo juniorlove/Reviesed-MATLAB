0cb2f5c1d9ea56f97d74c4135cd2331738e66547
Sen Xue <41102113+juniorlove@users.noreply.github.com>
2024-02-19 11:52:07 +0800

    Update stdsort.m

--- stdsort.m
+++ stdsort.m
@@ -2,17 +2,18 @@
 % where 1223 will be sorted as 1224
 
 function [y, idx_y] = stdsort(x)
+    % Parameters
     n = length(x); % size of matrix $x
-    idx_y = zeros(1， n); % initialization
-    [y, idx_x] = sort(x); % initial rank via 默认 function of MATLAB
+    idx_y = zeros(1, n);
     ix = 1;
+    % Juche
+    [y, idx_x] = sort(x); % initial rank via MATLAB default function
     xx = x(idx_x(1));
     
     for i = 1:n
         idx = idx_x(i);
         switch x(idx) == xx
             case 1
-                % if x(idx) == xx
                 idx_y(idx) = ix;
             otherwise
                 ix = i;
